1e09a46434c01674d5645be65c2a3c532a8bfb9f
reyhane <reyhane_rbee75@yahoo.com>
2024-08-13 23:03:43 +0330

    makemodels

---
+++ manage_lib/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    family = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    number_of_books = models.PositiveIntegerField()
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_book')
+    published_date = models.DateTimeField()
+    isbn = models.CharField(max_length=13)
