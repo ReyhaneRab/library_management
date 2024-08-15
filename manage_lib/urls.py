5d6bebdeb5620b90f95dec29c3a1503ab3af1839
reyhane <reyhane_rbee75@yahoo.com>
2024-08-15 13:46:28 +0330

    final

--- manage_lib/urls.py
+++ manage_lib/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('books/new/', views.book_create_update, name='book_create'),
-    path('books/<int:pk>/edit/', views.book_create_update, name='book_update'),  # این URL برای ویرایش کتاب است
+    path('books/<int:pk>/edit/', views.book_create_update, name='book_update'),
 ]
